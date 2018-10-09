8970cfbd9cf720604cdcca891f1892f6f11c2021
Masao SUGITA <masao.sugita@gmail.com>
2018-10-10 01:53:37 +0900

    'githubにあっぷする'

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 // app.js 
-var express = require('express');
-var Twitter = require('twitter');
-var CronJob = require("cron").CronJob;
+let express = require('express');
+let Twitter = require('twitter');
+let CronJob = require("cron").CronJob;
 
-var app = express();
+let app = express();
 
-var twitter = new Twitter({
-    consumer_key: 'rsZ80FOsOWmexeum9U8T5e92o',
-    consumer_secret: 'ZdRfoSwDlOq5I86XWNANeT6rA2Px41CqYjdDtLvltXEv8MMOdf',
-    access_token_key: '116741549-vNKcEQ41CFu7wE3PnAIWcnWJPZIwtw2YB04eMiq6',
-    access_token_secret: 'VAihSKde78bQHuW59g264c1h0C4S8yrae00uAVQXDJEhW'
+let twitter = new Twitter({
+    consumer_key: '*',
+    consumer_secret: '*',
+    access_token_key: '*',
+    access_token_secret: '*'
 });
 
-var cronTime = '0 * * * * *';
+let cronTime = '0 * * * * *';
 new CronJob({
     cronTime: cronTime,
     onTick: function () {
@@ -21,9 +21,14 @@ new CronJob({
     start: true
 });
 
+let tipsArray = [
+    "にゃー",
+    "わんわん",
+    "おっぱい",
+];
 
 function cycleTweet() {
-    let tips = 'ちょっと投稿テストです。';
+    let tips = tipsArray[Math.floor(Math.random() * tipsArray.length)];
 
     // 自動投稿 
     twitter.post('statuses/update', {status: tips}, (err, tweet, response) => {
